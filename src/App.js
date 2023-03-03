373148971173fffd4abc657ee15b2d24cb3f5aa1
JonLedo <jonledo.code@gmail.com>
2023-03-03 06:13:36 -0500

    created code for ther nav bar includign toggle function, and styles for wide screen and smaller sizes. Added a profile pic in assets folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Navbar from './pages/components/Navbar'
 import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Navbar />
+
+        <div className='content'>lorem1000</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
