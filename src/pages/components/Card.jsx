411e1d10cc7d30b6e6bbc2883c174773dfe7eed2
JonLedo <jonledo.code@gmail.com>
2023-03-10 06:49:57 -0500

    fixed issue by adding links to school/class sites inside the resume section. added that info into the data.js

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -3,7 +3,16 @@ const Card = ({ data }) => {
     <div className='card'>
       <p className='card-year'>{data.year}</p>
       <h4 className='card-subject'>{data.subject}</h4>
-      <p className='card-school red-text'>{data.school}</p>
+      <p className='card-school'>
+        <a
+          href={data.url}
+          target='_blank'
+          rel='noreferrer'
+          className=' red-text'
+        >
+          {data.school}
+        </a>
+      </p>
       <p className='card-desc'>{data.desc}</p>
     </div>
   )
