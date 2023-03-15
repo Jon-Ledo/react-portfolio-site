dfe9ed7a498905ebb5acbe5cbec252c079e79df3
JonLedo <jonledo.code@gmail.com>
2023-03-14 22:16:11 -0400

    netlify

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,19 +7,18 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const formData = new FormData(e.target)
+    const myForm = e.target
+    const formData = new FormData(myForm)
+
+    console.log(formData)
 
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => alert('Form submitted successfully!'))
+      .then(() => console.log('Form successfully submitted'))
       .catch((error) => alert(error))
-
-    setName('')
-    setEmail('')
-    setMessage('')
   }
 
   return (
